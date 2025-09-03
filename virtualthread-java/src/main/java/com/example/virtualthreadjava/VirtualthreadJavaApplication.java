5975c0e3ad41a2ae2febaa13abc2987b79d6bac9
taeyang121096 <taeyang121096@naver.com>
2025-09-04 00:58:15 +0900

    virtual thread project init

---
+++ virtualthread-java/src/main/java/com/example/virtualthreadjava/VirtualthreadJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtualthreadjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualthreadJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualthreadJavaApplication.class, args);
+    }
+
+}
---
+++ virtualthread-java/src/test/java/com/example/virtualthreadjava/VirtualthreadJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtualthreadjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualthreadJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
